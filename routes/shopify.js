5a38e0492df0c1a0102256f51ef9f0652c43b4cc
cyberabis <babishek@gmail.com>
2015-09-05 15:39:53 +0530

    shopify integration

--- routes/shopify.js
+++ routes/shopify.js
@@ -1,27 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var shopifyAPI = require('shopify-node-api');
+
+var shopify_api_key = process.env.SHOPIFY_API_KEY;
+var shopify_shared_secret = process.env.SHOPIFY_SHARED_SECRET;
+var redirect_uri = process.env.REDIRECT_URI;
+var shopify_scope = 'read_products,read_orders,write_orders,read_script_tags,write_script_tags,read_fulfillments,write_fulfillments';
 
 // define the home page route
 router.get('/:shop', function(req, res) {
 	var shop = req.params.shop;
-    
-    //Do something
-
     console.log('Got request from shop: ' + shop);
-    res.status(200).end();
 
+    //Do something
+    var Shopify = new shopifyAPI({
+        shop: shop, // MYSHOP.myshopify.com
+        shopify_api_key: shopify_api_key, // Your API key
+        shopify_shared_secret: shopify_shared_secret, // Your Shared Secret
+        shopify_scope: shopify_scope,
+        redirect_uri: redirect_uri
+    });
+    var auth_url = Shopify.buildAuthURL();
+	res.redirect(auth_url);
 });
 
 router.get('/oauth/callback/', function(req, res) {
 	var code = req.query.code;
+	var shop = req.query.shop;
+	console.log('Got code: ' + code + '|' + shop);
+
+	/*
 	var hmac = req.query.hmac;
 	var timestamp = req.query.timestamp;
 	var state = req.query.state;
-	var shop = req.query.shop;
+	*/
 
-    //Do something - Get permanent access token
-    console.log('Got code: ' + code);
-    res.status(200).end();
+	var query_params = req.query;
+	var Shopify = new shopifyAPI({
+        shop: shop, // MYSHOP.myshopify.com
+        shopify_api_key: shopify_api_key, // Your API key
+        shopify_shared_secret: shopify_shared_secret, // Your Shared Secret
+        shopify_scope: shopify_scope,
+        redirect_uri: redirect_uri
+    });
+
+    Shopify.exchange_temporary_token(query_params, function(err, data){
+	    // This will return successful if the request was authentic from Shopify
+	    // Otherwise err will be non-null.
+	    // The module will automatically update your config with the new access token
+	    // It is also available here as data['access_token']
+	    if(err == null) {
+	    	var access_token = data['access_token'];
+	    	console.log('Got permanent token: ' + access_token);
+	    	res.status(200).end();
+	    } else {
+	    	console.log('Could not exchange temp token');
+	    	res.status(401).end();
+	    }
+	});
 });
 
 module.exports = router;
