ee388b71fe0e00b53f78bc569a6f1f48d97841a6
cyberabis <babishek@gmail.com>
2015-09-07 14:36:47 +0530

    added https

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var https = require('https');
+
+var key = fs.readFileSync('./deliverytime-key.pem');
+var cert = fs.readFileSync('./deliverytime-cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -19,5 +29,6 @@ app.use(express.static('public'));
 var shopify = require('./routes/shopify');
 app.use('/shopify/', shopify);
 
-app.listen(port);
+//app.listen(port);
+server = https.createServer(https_options, app).listen(port);
 console.log('Delivery Time API is listening on port: ' + port);
