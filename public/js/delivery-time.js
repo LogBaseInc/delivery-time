55cf84acd470b915e06a65caf944c3404bf706e9
koukumar <kousik.kumar@gmail.com>
2015-10-30 11:17:16 +0530

    test

--- public/js/delivery-time.js
+++ public/js/delivery-time.js
@@ -240,7 +240,7 @@ if ($('#lbdt').length > 0) {
             var notes = $('#lbdt-city option:selected').text() + " | " + $('#lbdt-date option:selected').text()
                 + " | " + $('#lbdt-slots option:selected').text();
             shopifyDs['cartJson']['note'] = notes;
-            shopifyDs['cartJson']['attributes'] = myDateSelect.val().split(" ").join("");
+            shopifyDs['cartJson']['tags'] = myDateSelect.val().split(" ").join("");
             $.post('cart.js', shopifyDs['cartJson']);
             var query = "?city=" + shopifyDs['city'] +
                 "&date=" + myDateSelect.val().split(" ").join("") +
